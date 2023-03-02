e20364e009e82107e3e46876179aa2cb01a4f7c5
Gokhan <engokhangok@gmail.com>
2023-03-02 17:09:49 +0300

    Add project files.

---
+++ MvcProjectCamp/Controllers/TestController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MvcProjectCamp.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Test2()
+        {
+            return View();
+        }
+    }
+}
