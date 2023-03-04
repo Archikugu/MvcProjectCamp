292c1a0a73888311419d122211d7dcb829d8c4d5
Gokhan <engokhangok@gmail.com>
2023-03-04 14:16:29 +0300

    IRepository interface oluşturuldu ve manage nuget Ef paketleri yüklendi

---
+++ DataAccess/Abstract/ICategoryDal.cs
@@ -0,0 +1,19 @@
+﻿using Entity.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Abstract {
+    public interface ICategoryDal {
+        //Crud
+        //Type Name();
+
+        List<Category> categoryList();
+        void Insert(Category p);
+        void Update(Category p);
+        void Delete(Category p);
+
+    }
+}
---
+++ DataAccess/Concrete/Repositories/CategoryRepository.cs
@@ -0,0 +1,33 @@
+﻿using DataAccess.Abstract;
+using Entity.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.Repositories {
+    public class CategoryRepository : ICategoryDal {
+
+        Context context = new Context();
+        DbSet<Category> _object;
+        public List<Category> categoryList() {
+            return _object.ToList();
+        }
+
+        public void Delete(Category p) {
+            _object.Remove(p);
+            context.SaveChanges();
+        }
+
+        public void Insert(Category p) {
+            _object.Add(p);
+            context.SaveChanges();
+        }
+
+        public void Update(Category p) {
+            context.SaveChanges();
+        }
+    }
+}
