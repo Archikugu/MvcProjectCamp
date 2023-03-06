19166330addbfdcacc0fe4d4335fc8707bbb0fb1
Gokhan <engokhangok@gmail.com>
2023-03-06 20:07:27 +0300

    Kategorilerin Tabloda Listelenmesi

---
+++ Business/Concrete/CategoryManager.cs
@@ -0,0 +1,25 @@
+﻿using DataAccess.Concrete.Repositories;
+using Entity.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete {
+    public class CategoryManager {
+        GenericRepository<Category> repository = new GenericRepository<Category>();
+
+        public List<Category> GetAll() {
+            return repository.GetList();
+        }
+        public void Add(Category category) {
+            if (category.CategoryName == "" || category.CategoryName.Length <= 3 || category.CategoryDescription == "" || category.CategoryName.Length >= 50) {
+                //Hata mesajı 
+            }
+            else {
+                repository.Insert(category);
+            }
+        }
+    }
+}
---
+++ MvcProjectCamp/Controllers/CategoryController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MvcProjectCamp.Controllers {
+    public class CategoryController : Controller {
+        public IActionResult Index() {
+            return View();
+        }
+    }
+}
