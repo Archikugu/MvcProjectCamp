f9332d513a0374dc5cba297a20e2b8b7b1292fa1
Gokhan <engokhangok@gmail.com>
2023-03-08 19:05:23 +0300

    Kategorilerin Listelenmesi

--- MvcProjectCamp/Controllers/CategoryController.cs
+++ MvcProjectCamp/Controllers/CategoryController.cs
@@ -1,9 +1,15 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Business.Concrete;
+using Microsoft.AspNetCore.Mvc;
 
 namespace MvcProjectCamp.Controllers {
     public class CategoryController : Controller {
+        CategoryManager manager = new CategoryManager();
         public IActionResult Index() {
             return View();
+        }
+        public IActionResult GetCategoryList() {
+            var categoryValues = manager.GetAll();
+            return View(categoryValues);
         }
     }
 }
