33b3fcdad70cba685a13b147f5c99180463321b1
Gokhan <engokhangok@gmail.com>
2023-03-12 14:17:43 +0300

    Admin Kategori Ekleme eklendi

--- MvcProjectCamp/Controllers/AdminCategoryController.cs
+++ MvcProjectCamp/Controllers/AdminCategoryController.cs
@@ -1,8 +1,34 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Business.Concrete;
+using Business.ValidationRules;
+using DataAccess.EntityFramework;
+using Entity.Concrete;
+using FluentValidation.Results;
+using Microsoft.AspNetCore.Mvc;
 
 namespace MvcProjectCamp.Controllers {
     public class AdminCategoryController : Controller {
+        CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
         public IActionResult Index() {
+            var categoryValues = categoryManager.GetCategoryList();
+            return View(categoryValues);
+        }
+        [HttpGet]
+        public IActionResult AddCategory() {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddCategory(Category category) {
+            CategoryValidator categoryValidator = new CategoryValidator();
+            ValidationResult result = categoryValidator.Validate(category);
+            if (result.IsValid) {
+                categoryManager.CategoryAdd(category);
+                return RedirectToAction("Index");
+            }
+            else {
+                foreach (var item in result.Errors) {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
             return View();
         }
     }
