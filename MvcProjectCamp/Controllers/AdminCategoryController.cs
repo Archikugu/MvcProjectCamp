cabc202cb28997aff98da202633205cdd90ed4be
Gokhan <engokhangok@gmail.com>
2023-03-12 13:25:48 +0300

    Proje tema yğklemesi ve temanın düzeltmeleri eklendi

---
+++ MvcProjectCamp/Controllers/AdminCategoryController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MvcProjectCamp.Controllers {
+    public class AdminCategoryController : Controller {
+        public IActionResult Index() {
+            return View();
+        }
+    }
+}
