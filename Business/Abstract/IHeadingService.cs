c12a64c2a8e39186124626d5e977be455e992f7a
Gokhan <engokhangok@gmail.com>
2023-03-16 19:15:48 +0300

    Başlık İşlemleri Oluşturuldu

---
+++ Business/Abstract/IHeadingService.cs
@@ -0,0 +1,16 @@
+﻿using Entity.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Abstract {
+    public interface IHeadingService {
+        List<Heading> GetList();
+        void HeadingAdd(Heading heading);
+        void HeadingDelete(Heading heading);
+        void HeadingUpdate(Heading heading);
+        Heading GetById(int id);
+    }
+}
---
+++ MvcProjectCamp/Controllers/HeadingController.cs
@@ -0,0 +1,24 @@
+﻿using Business.Concrete;
+using DataAccess.EntityFramework;
+using Entity.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace MvcProjectCamp.Controllers {
+    public class HeadingController : Controller {
+        HeadingManager headingManager = new HeadingManager(new EfHeadingDal());
+        public IActionResult Index() {
+            var headingValues = headingManager.GetList();
+            return View(headingValues);
+        }
+        [HttpGet]
+        public IActionResult AddHeading() {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddHeading(Heading p) {
+            headingManager.HeadingAdd(p);
+            return RedirectToAction("Index");
+        }
+    }
+}
